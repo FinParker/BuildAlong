49d9c24bc548ce4eac94c89d276bca244209117a
iming <2576226012@qq.com>
2025-05-13 19:35:53 +0800

    try: try to use crate serde & serde_json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+/*
+ * @Author: iming 2576226012@qq.com
+ * @Date: 2025-05-13 18:43:55
+ * @LastEditors: iming 2576226012@qq.com
+ * @LastEditTime: 2025-05-13 19:27:21
+ * @FilePath: \BuildAlong\src\main.rs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+use anyhow::{Context, Error, Result};
+use clap::{Parser, Subcommand};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct BuildAlongState {
+    original_commits: Vec<String>,
+    current_step: usize,
+    my_branch_head: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let point = Point { x: 1, y: 2 };
+
+    // Convert the Point to a JSON string.
+    let serialized = serde_json::to_string(&point).unwrap();
+
+    // Prints serialized = {"x":1,"y":2}
+    println!("serialized = {}", serialized);
+
+    // Convert the JSON string back to a Point.
+    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
+
+    // Prints deserialized = Point { x: 1, y: 2 }
+    println!("deserialized = {:?}", deserialized);
 }
