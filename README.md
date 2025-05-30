05d075e4a319e37eedfb6eb98917015f308b3887
iming <2576226012@qq.com>
2025-05-30 17:02:54 +0800

    doc: add README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+<!--
+ * @Author: iming 2576226012@qq.com
+ * @Date: 2025-05-30 16:59:57
+ * @LastEditors: iming 2576226012@qq.com
+ * @LastEditTime: 2025-05-30 17:01:48
+ * @FilePath: \BuildAlong\README.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+# BuildAlong
+
+BuildAlong: 选择一个项目, 你也可以有自己的更酷实现!
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@
  * @Author: iming 2576226012@qq.com
  * @Date: 2025-05-13 18:43:55
  * @LastEditors: iming 2576226012@qq.com
- * @LastEditTime: 2025-05-13 19:27:21
+ * @LastEditTime: 2025-05-13 19:44:28
  * @FilePath: \BuildAlong\src\main.rs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 use anyhow::{Context, Error, Result};
 use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct BuildAlongState {
@@ -16,24 +17,68 @@ struct BuildAlongState {
     current_step: usize,
     my_branch_head: String,
 }
-#[derive(Serialize, Deserialize, Debug)]
-struct Point {
-    x: i32,
-    y: i32,
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
 }
 
 fn main() {
-    let point = Point { x: 1, y: 2 };
+    let cli = Cli::parse();
 
-    // Convert the Point to a JSON string.
-    let serialized = serde_json::to_string(&point).unwrap();
+    // You can check the value provided by positional arguments, or option arguments
+    if let Some(name) = cli.name.as_deref() {
+        println!("Value for name: {name}");
+    }
 
-    // Prints serialized = {"x":1,"y":2}
-    println!("serialized = {}", serialized);
+    if let Some(config_path) = cli.config.as_deref() {
+        println!("Value for config: {}", config_path.display());
+    }
 
-    // Convert the JSON string back to a Point.
-    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
+    // You can see how many times a particular flag or argument occurred
+    // Note, only flags can have multiple occurrences
+    match cli.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
 
-    // Prints deserialized = Point { x: 1, y: 2 }
-    println!("deserialized = {:?}", deserialized);
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(Commands::Test { list }) => {
+            if *list {
+                println!("Printing testing lists...");
+            } else {
+                println!("Not printing testing lists...");
+            }
+        }
+        None => {}
+    }
+
+    // Continued program logic goes here...
 }
